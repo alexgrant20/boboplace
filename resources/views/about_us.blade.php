27a185758d83376bca23198c73af4a70805c57a1
Padmawati Pramita <padmawati.pramita@binus.edu>
2022-12-26 20:01:17 +0700

    adding about us page

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -5,4 +5,19 @@
 @endsection
 
 @section('content')
+   <div style="border-radius: 15px; border: 0.2px solid black; padding: 20px;">
+      <div>
+         <h2>About Us</h2>
+      </div>
+      <hr>
+      <div style="font-size: 20px">
+         <div>Project Member Team:</div>
+         <br>
+         <div>-	2440027631 - Dicky Hung</div>
+         <div>-	2440007762 - Francesco Jovan</div>
+         <div>-	2440027676 - Ghaly Zahirsyah</div>
+         <div>-	2440106522 - Padmawati Pramita</div>
+         <div>-	2401960422 - Steven Alexander Grant</div>
+      </div>
+   </div>
 @endsection
